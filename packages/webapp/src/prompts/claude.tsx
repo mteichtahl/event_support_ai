ba93f34ec5da34cb929419a2f06e10d4bc82f53c
fsatsuki <fsatsuki@amazon.co.jp>
2024-04-10 06:24:06 +0000

    プロンプト修正

--- packages/webapp/src/prompts/claude.tsx
+++ packages/webapp/src/prompts/claude.tsx
@@ -6,7 +6,7 @@ import {
 export const claudePrompter: Prompter = {  
   summarizePrompt(params: SummarizeParams): string {
     return `あなたは日英同時通訳の支援をするエージェントです。
-現在進行中のイベントのスピーチの履歴が与えられます。
+現在進行中のイベントのスピーチの履歴が与えられます。履歴は下に行くほど新しいです。
 直近のスピーチの内容を詳細に説明してください。
 
 日本語で回答してください。
@@ -18,7 +18,7 @@ ${params.sentence}
 親切な挨拶や返答は不要です。
 
 
-Assistant: 
+\n\nAssistant: 
 `;
   },
 
